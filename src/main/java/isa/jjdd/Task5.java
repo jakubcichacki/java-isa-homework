427d244f81347a60a406d7f9f3c771108495bbfb
lukjel <lukjel@users.noreply.github.com>
2020-04-22 22:59:02 +0200

    Task 5 - homework - Initial commit.

---
+++ src/main/java/isa/jjdd/Task5.java
@@ -0,0 +1,33 @@
+package isa.jjdd;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Task5 {
+
+	private static final Logger STDOUT = LoggerFactory.getLogger("CONSOLE_OUT");
+
+	public void run() {
+		//All tests have to pass
+
+		if (longestWord("Jaka piękna dziś#$%!@ pogoda!!!") == 6) {
+			STDOUT.info("1 - OK");
+		} else {
+			STDOUT.info("1 - Wrong");
+		}
+
+		if (longestWord("Dzisiaj pracowaliśmy nad Projekt1029384983.") == 17) {
+			STDOUT.info("1 - OK");
+		} else {
+			STDOUT.info("1 - Wrong");
+		}
+	}
+
+	private int longestWord(String sentence) {
+		// Mamy podane zdanie.
+		// Naszym zadaniem jest zwrócenie długości najdłuższego słowa
+		// w danym zdaniu. Uwaga! Bierzemy pod uwagę tylko litery
+		// i cyfry. Znaki interpunkcyjne i inne pomijamy.
+		return 0;
+	}
+}
